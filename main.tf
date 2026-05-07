aa3e5fecbdd0c4c380084a4b7d52703ee9cff692
oliviasiow <olivia.siow.y@gmail.com>
2026-05-07 23:23:54 +0800

    Change main osy5

--- main.tf
+++ main.tf
@@ -11,28 +11,26 @@ terraform {
       version = "~> 5.0"
     }
   }
-}
 
-terraform {
   backend "s3" {
-    bucket = "sctp-ce12-tfstate-bucket" # Change this
-    key    = "osy-ce12m3l2.tfstate"     # Change this
+    bucket = "sctp-ce12-tfstate-bucket"
+    key    = "osy-ce12m3l2.tfstate"
     region = "ap-southeast-1"
   }
 }
 
+# Primary S3 bucket
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix = "osy-ce12m3l2-bucket" # Set your bucket name here
+  bucket = "osy-ce12m3l2-bucket"
 }
 
+# Event notifications (EventBridge)
 resource "aws_s3_bucket_notification" "s3_events" {
-  bucket = aws_s3_bucket.s3_tf.id
-
-  eventbridge {
-    events = ["s3:ObjectCreated:*"]
-  }
+  bucket      = aws_s3_bucket.s3_tf.id
+  eventbridge = true
 }
 
+# Public access block
 resource "aws_s3_bucket_public_access_block" "s3_block" {
   bucket                  = aws_s3_bucket.s3_tf.id
   block_public_acls       = true
@@ -41,6 +39,7 @@ resource "aws_s3_bucket_public_access_block" "s3_block" {
   restrict_public_buckets = true
 }
 
+# Lifecycle configuration
 resource "aws_s3_bucket_lifecycle_configuration" "s3_lifecycle" {
   bucket = aws_s3_bucket.s3_tf.id
 
@@ -48,27 +47,17 @@ resource "aws_s3_bucket_lifecycle_configuration" "s3_lifecycle" {
     id     = "expire-old-objects"
     status = "Enabled"
 
+    filter {
+      prefix = "" # applies to all objects
+    }
+
     expiration {
       days = 30
     }
   }
 }
 
-resource "aws_s3_bucket_replication_configuration" "s3_replication" {
-  bucket = aws_s3_bucket.s3_tf.id
-  role   = aws_iam_role.replication_role.arn
-
-  rule {
-    id     = "replicate-all"
-    status = "Enabled"
-
-    destination {
-      bucket        = aws_s3_bucket.replica.arn
-      storage_class = "STANDARD"
-    }
-  }
-}
-
+# Versioning
 resource "aws_s3_bucket_versioning" "s3_versioning" {
   bucket = aws_s3_bucket.s3_tf.id
 
@@ -77,6 +66,7 @@ resource "aws_s3_bucket_versioning" "s3_versioning" {
   }
 }
 
+# Encryption with KMS
 resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
   bucket = aws_s3_bucket.s3_tf.id
 
@@ -95,16 +85,81 @@ resource "aws_kms_key" "s3_key" {
 
 # Logging target bucket
 resource "aws_s3_bucket" "s3_logs" {
-  bucket_prefix = "osy-ce12m3l2-logs"
+  bucket = "osy-ce12m3l2-logs"
 }
 
-# Attach logging to your main bucket
+# Attach logging to main bucket
 resource "aws_s3_bucket_logging" "s3_logging" {
-  bucket = aws_s3_bucket.s3_tf.id
-
+  bucket        = aws_s3_bucket.s3_tf.id
   target_bucket = aws_s3_bucket.s3_logs.id
   target_prefix = "log/"
 }
 
+# Destination bucket in another region for replication
+provider "aws" {
+  alias  = "replica"
+  region = "ap-northeast-1"
+}
 
+resource "aws_s3_bucket" "replica" {
+  provider = aws.replica
+  bucket   = "osy-ce12m3l2-replica"
+}
 
+# IAM role for replication
+resource "aws_iam_role" "replication_role" {
+  name = "s3-replication-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "s3.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "replication_policy" {
+  role = aws_iam_role.replication_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:GetReplicationConfiguration",
+          "s3:ListBucket",
+          "s3:GetObjectVersion",
+          "s3:GetObjectVersionAcl",
+          "s3:GetObjectVersionForReplication"
+        ]
+        Resource = [aws_s3_bucket.s3_tf.arn, "${aws_s3_bucket.s3_tf.arn}/*"]
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["s3:ReplicateObject", "s3:ReplicateDelete", "s3:ReplicateTags"]
+        Resource = [aws_s3_bucket.replica.arn, "${aws_s3_bucket.replica.arn}/*"]
+      }
+    ]
+  })
+}
+
+# Replication configuration
+resource "aws_s3_bucket_replication_configuration" "s3_replication" {
+  bucket = aws_s3_bucket.s3_tf.id
+  role   = aws_iam_role.replication_role.arn
+
+  rule {
+    id     = "replicate-all"
+    status = "Enabled"
+
+    destination {
+      bucket        = aws_s3_bucket.replica.arn
+      storage_class = "STANDARD"
+    }
+  }
+}
