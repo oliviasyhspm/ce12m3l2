b83632da1039725308a5214b631a2c319c5e18bb
oliviasiow <olivia.siow.y@gmail.com>
2026-05-07 23:28:48 +0800

    Change main osy6

--- main.tf
+++ main.tf
@@ -129,8 +129,8 @@ resource "aws_iam_role_policy" "replication_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Effect   = "Allow"
-        Action   = [
+        Effect = "Allow"
+        Action = [
           "s3:GetReplicationConfiguration",
           "s3:ListBucket",
           "s3:GetObjectVersion",
