72c5803eb4c84e4686fc0576512b4b1c88374fb8
oliviasiow <olivia.siow.y@gmail.com>
2026-05-07 21:23:57 +0800

    Change main format new osy2

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce12-tfstate-bucket" # Change this
-    key    = "osy-ce12m3l2.tfstate"  # Change this
+    key    = "osy-ce12m3l2.tfstate"     # Change this
     region = "ap-southeast-1"
   }
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix = "osy-ce12m3l2-bucket"  # Set your bucket name here
+  bucket_prefix = "osy-ce12m3l2-bucket" # Set your bucket name here
 }
